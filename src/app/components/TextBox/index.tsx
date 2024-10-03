adf658abcafa63aa73bb1e50d75da968043185f0
mkaiho <mkaiho.cgengo@gmail.com>
2024-10-03 15:15:16 +0000

    add storybook dependency

---
+++ src/app/components/TextBox/index.tsx
@@ -0,0 +1,27 @@
+import { FC, MouseEvent, ReactNode, useRef, useState } from "react"
+
+interface Props {
+  children?: ReactNode
+  type?: "button" | "reset" | "submit"
+  onClick: (
+    event?: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+  ) => void
+}
+
+const Button: FC<Props> = ({ children, type, onClick }: Props) => {
+  const handleClick = (
+    event?: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+  ) => {
+    onClick(event)
+  }
+
+  return (
+    <>
+      <button type={type} onClick={(e) => handleClick(e)}>
+        {children}
+      </button>
+    </>
+  )
+}
+
+export default Button
